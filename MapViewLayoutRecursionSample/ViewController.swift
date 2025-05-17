1117c9d6a641a6ee6200450b51d0a7bbdd595e01
drypot <drypot@gmail.com>
2025-05-17 23:24:50 +0900

    first commit

---
+++ MapViewLayoutRecursionSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MapViewLayoutRecursionSample
+//
+//  Created by Kyuhyun Park on 5/17/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# MapView Layout Recursion Sample
+
+MapView Layout Recursion 오류 샘플.
+
+## License
+
+[MIT](LICENSE)
